ec6286c8127a00b1f573a1f6031124d9f6068e53
arnav-verma22 <arn.verma22@gmail.com>
2020-11-25 22:06:20 +0530

    shifting outer elements

--- HackerRank/matrix-layer-rotation.py
+++ HackerRank/matrix-layer-rotation.py
@@ -13,7 +13,24 @@ def matrixRotation(matrix, r):
     rows = len(matrix)
     columns = len(matrix[0])
 
+    i = 0
+    j = 0
+    temp = matrix[0][0]
+    while j < columns - 1:
+        matrix[i][j] = matrix[i][j + 1]
+        j += 1
+    while i < rows - 1:
+        matrix[i][j] = matrix[i + 1][j]
+        i += 1
+    while j > 0:
+        matrix[i][j] = matrix[i][j - 1]
+        j -= 1
+    while i > 0:
+        matrix[i][j] = matrix[i - 1][j]
+        i -= 1
+    matrix[i + 1][j] = temp
 
+    print(matrix)
 
 
 if __name__ == '__main__':
