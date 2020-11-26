4f2090b6855744b564afd5c6c40b82a2a9f123c8
arnav-verma22 <arn.verma22@gmail.com>
2020-11-26 09:45:17 +0530

    using function

--- HackerRank/matrix-layer-rotation.py
+++ HackerRank/matrix-layer-rotation.py
@@ -24,30 +24,20 @@ def rotate(matrix, k, l, r, c):
 
 # Complete the matrixRotation function below.
 def matrixRotation(matrix, r):
-    mat = matrix[1:3][1:3]
-    print(mat)
-    print(matrix)
-    rows = len(matrix)
-    columns = len(matrix[0])
+    # print(matrix)
 
-    i = 0
-    j = 0
-    temp = matrix[0][0]
-    while j < columns - 1:
-        matrix[i][j] = matrix[i][j + 1]
-        j += 1
-    while i < rows - 1:
-        matrix[i][j] = matrix[i + 1][j]
-        i += 1
-    while j > 0:
-        matrix[i][j] = matrix[i][j - 1]
-        j -= 1
-    while i > 0:
-        matrix[i][j] = matrix[i - 1][j]
-        i -= 1
-    matrix[i + 1][j] = temp
+        rows = len(matrix)
+        columns = len(matrix[0])
+        i = 0
+        j = 0
+        while i < rows and j < columns:
+            rotate(matrix, i, j, rows, columns)
+            i += 1
+            j += 1
+            rows -= 1
+            columns -= 1
 
-    print(matrix)
+
 
 
 if __name__ == '__main__':
