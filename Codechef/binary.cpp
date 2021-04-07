b0b036ae49940944c1da86e95b4a7e5edee40a3c
arnav-verma22 <arn.verma22@gmail.com>
2021-04-07 18:13:42 +0530

    subseq

--- Codechef/binary.cpp
+++ Codechef/binary.cpp
@@ -1,21 +1,52 @@
-#include<iostream>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
-/*int binary(string s)
+int binary(int x)
 {
-    int bl[9];
+    int bl[9] = {0};
+    while(x != 0)
+    {
+        int y = floor(log2(x));
+        bl[8 - y] = 1;
+        x = x - pow(2, y);   
+    }
+    stringstream ss;
+    for (unsigned i = 0; i < 9; ++i)
+        ss << bl[i];
+    ss >> x;
+    return x;
+}
 
-
-}*/
+bool isSubSequence(char str1[], char str2[],
+                                 int m, int n)
+{
+     
+    // Base Cases
+    if (m == 0)
+        return true;
+    if (n == 0)
+        return false;
+ 
+    // If last characters of two
+    // strings are matching
+    if (str1[m - 1] == str2[n - 1])
+        return isSubSequence(str1, str2, m - 1, n - 1);
+ 
+    // If last characters are
+    // not matching
+    return isSubSequence(str1, str2, m, n - 1);
+}
 
 int main()
 {
-    string name, surname;
-    int bl[9] = {0};
-    cin>>name>>surname;
-    cout<<name<<surname;
-    for(int i = 0; i < 9; i++)
-    {
-        cout<<bl[i]<<"\n";
-    }
+    //string name, surname;
+    //int bl[9] = {0};
+    //cin>>name>>surname;
+    for(int i; i < 20; i++)
+        cout<<i<<" - "<<binary(i)<<endl;
+    
+    
+    
 }
