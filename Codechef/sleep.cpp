22b85777acb5d5ac863a0d72e9b6b21eb4e5d453
arnav-verma22 <arn.verma22@gmail.com>
2021-06-27 17:40:46 +0530

    practicing

---
+++ Codechef/sleep.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<n;
+    string s;
+    for(int i = 0;i < n;i++)
+    {
+        int l, h;
+        cin>>l;
+        cin>>h;        
+    }
+}
