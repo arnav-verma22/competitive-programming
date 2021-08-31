4fc9fca2c98c9fee7b99b755038dc236d4fee34b
arnav-verma22 <arn.verma22@gmail.com>
2021-08-31 10:02:53 +0530

    permutations

---
+++ DSA/recursion/permutations.java
@@ -0,0 +1,22 @@
+//package recursion;
+import java.util.*;
+
+public class permutations {
+    static void permutation(String s, String ans)
+    {
+        if(s.length() == 0)
+            System.out.println(ans);
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            char fix = s.charAt(i);
+            String ros = s.substring(0,i) + s.substring(i+1);
+            permutation(ros, ans+fix);
+        }
+    }
+    public static void main(String args[])
+    {
+        String s = "abc";
+        permutation(s, "");
+    }
+}
